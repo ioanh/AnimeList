458fd11bb1643f71fd211a54608857a46d4f2836
ioanh <ioan.xristov@abv.bg>
2021-06-13 00:19:14 +0300

    creating the home component and the api service

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable ()
+export class api {
+    constructor(private httpClient: HttpClient) {}
+    queryChanged = new EventEmitter<string>();
+    getApi(query: string = 'Death%20Note'){
+        return this.httpClient.get(`https://api.jikan.moe/v3/search/anime?q=${query}&page=1`)
+    }
+}
