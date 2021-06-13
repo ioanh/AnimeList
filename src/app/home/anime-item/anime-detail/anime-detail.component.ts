5542015fa888395077d85b48195a60c3b446c82b
ioanh <ioan.xristov@abv.bg>
2021-06-13 23:44:53 +0300

    making the detail load only if there is data passed to the component

--- src/app/home/anime-item/anime-detail/anime-detail.component.ts
+++ src/app/home/anime-item/anime-detail/anime-detail.component.ts
@@ -12,30 +12,13 @@ export class AnimeDetailComponent implements OnInit {
   constructor(private apiService: api) { }
 
   anime: Anime;
-  animeTitle: string = '';
-  animeImageURL: string = '';
-  animeURL: string = '';
-  episodes: number = 0;
-  score: number = 0;
-  startDate: string = '';
-  endDate: string = '';
-  rated: string = '';
-  members: number = 0;
+  dataReceived: boolean;
 
-
-  ngOnInit(): void {
+  ngOnInit(){
     this.apiService.animeSelected.subscribe(
       (data) => {
         this.anime = data;
-        this.animeTitle = this.anime.title
-        this.animeImageURL = this.anime.image_url
-        this.animeURL = this.anime.url
-        this.episodes = this.anime.episodes
-        this.score = this.anime.score
-        this.startDate = this.anime.start_date
-        this.endDate = this.anime.end_date
-        this.rated = this.anime.rated
-        this.members = this.anime.members
+        this.dataReceived = true;
       }
     )
   }
