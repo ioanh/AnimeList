0fde3c793a626cee62074649938c6b123c628e5a
ioanh <ioan.xristov@abv.bg>
2021-06-13 12:12:32 +0300

    making the select anime detail work by predefining values so i won`t get error undefined
    +todo has to implement the routert so we can switch from selected anime to animelist

--- src/app/home/anime-item/anime-detail/anime-detail.component.ts
+++ src/app/home/anime-item/anime-detail/anime-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { api } from 'src/app/shared/api.service';
+import { Anime } from 'src/app/shared/anime.model';
 
 @Component({
   selector: 'app-anime-detail',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimeDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: api) { }
+
+  anime: Anime;
+  animeTitle: string = '';
+  animeImageURL: string = '';
+  animeURL: string = '';
+  episodes: number = 0;
+  score: number = 0;
+  startDate: string = '';
+  endDate: string = '';
+  rated: string = '';
+  members: number = 0;
+
 
   ngOnInit(): void {
+    this.apiService.animeSelected.subscribe(
+      (data) => {
+        this.anime = data;
+        this.animeTitle = this.anime.title
+        this.animeImageURL = this.anime.image_url
+        this.animeURL = this.anime.url
+        this.episodes = this.anime.episodes
+        this.score = this.anime.score
+        this.startDate = this.anime.start_date
+        this.endDate = this.anime.end_date
+        this.rated = this.anime.rated
+        this.members = this.anime.members
+      }
+    )
   }
 
 }
